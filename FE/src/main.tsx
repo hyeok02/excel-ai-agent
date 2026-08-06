eae8917536b4d592753c1ea25a80f51d3c623000
ysh2002m <ajin1991@naver.com>
2026-08-06 11:06:00 +0900

    fix: update browser tab title

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -5,6 +5,8 @@ import { createRoot } from 'react-dom/client'
 
 import App from '@/App'
 
+document.title = 'Decision Support System'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
